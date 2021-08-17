bfd5c075a0f03b4884d8168533a50a5fb3af83fd
Brandon Lim <limws.brandon@gmail.com>
2021-08-17 23:50:43 +0800

    Implement calling API and process JSON response.

--- SmartLight/SmartLight.ino
+++ SmartLight/SmartLight.ino
@@ -1,6 +1,7 @@
 
 #include <Adafruit_NeoPixel.h>
 #include <WiFiNINA.h>
+#include <ArduinoJson.h>
 
 #define LED_PIN 2
 #define LED_COUNT 8
@@ -11,22 +12,27 @@ bool toggled = false;
 
 uint8_t LEDColor[3] = {0, 0, 0};
 
-const char *server = "192.168.10.143";
-const char *ssid_name = "";
-const char *ssid_pass = "";
+const char *server = "*";
+const char *ssid_name = "*";
+const char *ssid_pass = "*";
 
 const String api = "/api/v1/index";
+const String lightConfigApi = "/api/v1/lights/config";
 
 WiFiClient client;
 int wifiStatus = WL_IDLE_STATUS;
 
 void setup() {
 
+  stick.begin();
+  stick.show();
+
   Serial.begin(9600);
+
   //while (!Serial);
   Serial.print("Initialising...");
   Serial.println();
-  
+
   while (wifiStatus != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid_name);
@@ -36,38 +42,103 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.print("Connecting to: ");
   Serial.println( server);
-  if (client.connect(server, 3000)) {
 
-    Serial.println("Connected to server");
 
-    client.println("GET "+api+" HTTP/1.1");
-    client.println("Connection: close");
-    client.println();
-    Serial.println("API called.");
-    Serial.println(client);
-  }
-  else
-  {
+  if (!client.connect(server, 3000)) {
     Serial.println("Fail to connect to server!");
     return;
-  }
 
-  stick.begin();
-  stick.show();
+  }
+  Serial.println("Checking server is up!");
+  pingServer();
 }
 
 void loop() {
 
-  while (client.available()) {
-    char c = client.read();
-    Serial.print(c);
-  }
-  Serial.println();
+  fetchLightConfiguration();
   if (!client.connected()) {
     Serial.println("Client disconnected");
     client.stop();
+  }
+  delay(10000);
+}
 
-    for(;;);
+void pingServer() {
+
+  Serial.println("Connecting to server at " + api);
+
+  client.println("GET " + api + " HTTP/1.1");
+  client.println("Connection: close");
+  client.println();
+  Serial.println("API called.");
+  Serial.println(client);
+  delay(10);
+  while (client.connected()) {
+    if (client.available()) {
+      String c = client.readStringUntil('\0');
+      Serial.print(c);
+      Serial.println();
+      break;
+    }
+  }
+
+}
+
+void fetchLightConfiguration() {
+
+  if (client.connect(server, 3000)) {
+    Serial.println("Connected to server at " + lightConfigApi);
+    client.println("GET " + lightConfigApi + " HTTP/1.1" );
+    client.println("Connection: close");
+    client.println();
+    Serial.println("Called " + lightConfigApi);
+
+    delay(10);
+
+    while (client.connected()) {
+      if (client.available()) {
+
+        String c = client.readStringUntil('\0');
+        //Get rid of HTTP Response header
+        c.remove(0, c.indexOf("\r\n\r\n") + 1);       
+        
+        processJsonResponse(c);
+
+        break;
+      }
+    }
+  }
+}
+
+void processJsonResponse(String response) {
+
+  Serial.println("Processing server response: ");
+  Serial.println(response);
+  DynamicJsonDocument doc(2048);
+
+  DeserializationError error = deserializeJson(doc, response);
+  if (error) {
+    Serial.println("Deserialisation failed.");
+    Serial.println(error.f_str());
+    return;
+  }
+  Serial.println("Deserialise success...");
+  JsonObject content = doc.as<JsonObject>();
+
+  String name1 = content["name"].as<String>();
+  Serial.println("Name: " + name1);
+
+  JsonArray pixels = content["pixels"].as<JsonArray>();
+  Serial.print("Pixel Array: ");
+  Serial.println(pixels.size());
+  for (JsonVariant pixel : pixels) {
+    JsonObject led = pixel.as<JsonObject>();
+
+    Serial.print("Red: ");
+    Serial.println(led["red"].as<int>());
+
+    Serial.print("Green: ");
+    Serial.println(led["green"].as<int>());
   }
 
 
